3b7ca81ad5aed34adbe6d1c8fc89132462b21311
glebosotov <gleb.osotov@gmail.com>
2025-05-23 13:01:32 +0300

    feat: add openai and logging instruments

--- app/telemetry.py
+++ app/telemetry.py
@@ -1,12 +1,15 @@
 """Telemetry configuration."""
 
-import logging
-
 from opentelemetry import trace
 from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
 
-logging.basicConfig(level=logging.INFO)
 if not HTTPXClientInstrumentor().is_instrumented_by_opentelemetry:
     HTTPXClientInstrumentor().instrument()
+if not OpenAIInstrumentor().is_instrumented_by_opentelemetry:
+    OpenAIInstrumentor().instrument()
+if not LoggingInstrumentor().is_instrumented_by_opentelemetry:
+    LoggingInstrumentor().instrument(set_logging_format=True)
 
 tracer = trace.get_tracer("main.tracer")
